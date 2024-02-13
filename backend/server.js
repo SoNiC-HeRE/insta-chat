0def395895f0f3293b2dc4c0d24ff9d52aa78acd
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2024-02-13 20:58:25 +0530

    Initialise basic socket.io functionality with cors along with ENV Config for PORT

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,35 @@
 import express from 'express';
-import { createServer } from 'node:http';
+import { createServer } from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
 
 const app = express();
+app.use(cors());
+dotenv.config();
+
 const server = createServer(app);
+const PORT = process.env.PORT || 3000;
+
+const io = new Server(server,{
+    cors: {
+        origin: `http://localhost:${PORT}`,
+        methods: ['GET', 'POST'],
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+server.listen(PORT, () => {
+  console.log(`Server running at PORT:${PORT}`);
 });
