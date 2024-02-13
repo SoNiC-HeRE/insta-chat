33e74fec902be908d4f84c90658377504df8fd4b
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2024-02-14 02:17:39 +0530

    Completed full functionality

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -12,6 +12,8 @@ function Chat({socket, userName, room}) {
             time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
         }
         await socket.emit('send_message', messageData);
+        setmessageList((list)=>[...list, messageData])
+        setcurrentMessage("");
     }
   };
 
@@ -32,12 +34,25 @@ function Chat({socket, userName, room}) {
         </div>
         <div className="chat-body">
           {messageList.map((messageContent)=>{
-            return <h1>{messageContent.message}</h1>
+            return (
+              <div className='message' id={userName === messageContent.author ? "you" : "other"}>
+                <div>
+                  <div className='message-content'>
+                      <p>{messageContent.message}</p>
+                  </div>
+                  <div className='message-meta'>
+                      <p id="time">{messageContent.time}</p>
+                      <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
           })}
         </div>
         <div className="footer">
             <input 
                 type="text" 
+                value={currentMessage}
                 placeholder="Type a message..." 
                 onChange={(e)=> setcurrentMessage(e.target.value)}
             />
