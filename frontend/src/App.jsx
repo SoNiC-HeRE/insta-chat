aa341995c60c7489b7f744b45bc15359ecc2c2cd
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2024-02-14 22:51:17 +0530

    Add animations for home UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import io from "socket.io-client";
 import Chat from "./Chat";
 import { PiHandWaving } from "react-icons/pi";
 import Marquee from "./components/Marquee/Marquee";
+import { motion, useAnimation } from "framer-motion";
 
 const socket = io.connect("http://localhost:3000");
 const room = "Development";
 
 function App() {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start({
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        ease: "easeInOut",
+      },
+    });
+  }, [controls]);
+
   const [userName, setuserName] = useState("");
   const [showChat, setshowChat] = useState(false);
 
@@ -24,28 +38,41 @@ function App() {
       {!showChat ? (
         <div className="main">
           <div className="glass-container">
-          <div className="hero-container">
-            <div className="hero-left">
-              <h3 className="heading">Hello <span className="icon"><PiHandWaving size={120}/></span></h3>
-              <p className="sub-heading">Welcome to Insta Chat</p>
+            <div className="hero-container">
+              <motion.div
+                initial={{ x: -100, opacity: 0 }}
+                animate={controls}
+                className="hero-left"
+              >
+                <h3 className="heading">
+                  Hello{" "}
+                  <span className="icon">
+                    <PiHandWaving size={120} />
+                  </span>
+                </h3>
+                <p className="sub-heading">Welcome to Insta Chat</p>
+              </motion.div>
+              <motion.div
+                initial={{ x: -100, opacity: 0 }}
+                animate={controls}
+                className="hero-right"
+              >
+                <p>Enter a username and hop in!</p>
+                <div className="container">
+                  <input
+                    className="input-box"
+                    type="text"
+                    placeholder="Your name goes here"
+                    value={userName}
+                    onChange={(e) => setuserName(e.target.value)}
+                  />
+                  <button className="button" onClick={joinRoom}>
+                    <span class="label">Let's Chat</span>
+                  </button>
+                </div>
+              </motion.div>
             </div>
-            <div className="hero-right">
-              <p>Enter a username and hop in!</p>
-              <div className="container">
-                <input
-                  className="input-box"
-                  type="text"
-                  placeholder="Your name goes here"
-                  value={userName}
-                  onChange={(e) => setuserName(e.target.value)}
-                />
-                <button className="button" onClick={joinRoom}>
-                  <span class="label">Let's Chat</span>
-                </button>
-              </div>
-            </div>
-          </div>
-          <Marquee/>
+            <Marquee />
           </div>
         </div>
       ) : (
--- frontend/src/components/Marquee/Marquee.jsx
+++ frontend/src/components/Marquee/Marquee.jsx
@@ -1,23 +1,43 @@
 import { motion } from "framer-motion";
 import React from "react";
-import './Marquee.css';
+import "./Marquee.css";
 
 function Marquee() {
   return (
-      <div className="text-container">
-        <motion.h1 initial={{x: 0}} animate={{x: '-100%'}} transition={{ repeat: Infinity, ease: 'linear', duration: 4}} className='text'>
-          Designed and Developed by Sonic
-        </motion.h1>
-        <motion.h1 initial={{x: 0}} animate={{x: '-100%'}} transition={{ repeat: Infinity, ease: 'linear', duration: 4}} className='text'>
-          Designed and Developed by Sonic
-        </motion.h1>
-        <motion.h1 initial={{x: 0}} animate={{x: '-100%'}} transition={{ repeat: Infinity, ease: 'linear', duration: 4}} className='text'>
-          Designed and Developed by Sonic
-        </motion.h1>
-        <motion.h1 initial={{x: 0}} animate={{x: '-100%'}} transition={{ repeat: Infinity, ease: 'linear', duration: 4}} className='text'>
-          Designed and Developed by Sonic
-        </motion.h1>
-      </div>
+    <div className="text-container">
+      <motion.h1
+        initial={{ x: 0 }}
+        animate={{ x: "-100%" }}
+        transition={{ repeat: Infinity, ease: "linear", duration: 4 }}
+        className="text"
+      >
+        Designed and Developed by Sonic
+      </motion.h1>
+      <motion.h1
+        initial={{ x: 0 }}
+        animate={{ x: "-100%" }}
+        transition={{ repeat: Infinity, ease: "linear", duration: 4 }}
+        className="text"
+      >
+        Designed and Developed by Sonic
+      </motion.h1>
+      <motion.h1
+        initial={{ x: 0 }}
+        animate={{ x: "-100%" }}
+        transition={{ repeat: Infinity, ease: "linear", duration: 4 }}
+        className="text"
+      >
+        Designed and Developed by Sonic
+      </motion.h1>
+      <motion.h1
+        initial={{ x: 0 }}
+        animate={{ x: "-100%" }}
+        transition={{ repeat: Infinity, ease: "linear", duration: 4 }}
+        className="text"
+      >
+        Designed and Developed by Sonic
+      </motion.h1>
+    </div>
   );
 }
 
